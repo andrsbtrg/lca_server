6751a89f68127069f8982927ae10bf6f8873e04b
buitrago <buitrago@imagine-computation.com>
2022-04-18 00:19:25 +0200

    renamed file to LCA

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import json
 
-from probLCA import material, assembly
+from LCA import material, assembly
 from flask   import jsonify, request
 
 from app import app
